f4b99a727493f7438eb65001c843c976befb4f88
yangjiaxun <yangjiaxun@longfor.com>
2021-08-25 16:06:04 +0800

    test

--- src/common/external-links/CopyRight.tsx
+++
@@ -1,6 +0,0 @@
-import React, { FC } from 'react';
-
-export const CopyRight: FC = () => {
-  const currentYears = new Date().getFullYear();
-  return <span>2021-{currentYears} xunserver 出品</span>;
-};
